79b830d5ecd737e8fa8a92ba8e9884cf054cd4fc
Mayank Kumar Sharma <pigamers1123@gmail.com>
2024-06-06 17:28:58 +0000

    updated ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-    <div class="bg-two mt-24 py-32">
+    <div class="bg-two mt-24 py-32 font-fonty">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-three md:text-5xl lg:text-6xl">About Me</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Welcome to the About Page</p>
